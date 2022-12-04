ce47d5fc4e1259ed3536b901bfc131cc1724fa39
Panbrawler <109430498+panbrawl91@users.noreply.github.com>
2022-12-04 17:07:07 +0300

    Add files via upload
    draw

---
+++ Art in Python.py
@@ -0,0 +1,48 @@
+from turtle import *
+shape("circle")
+x = numinput("X-фигура", "Введите X фигуру/Enter X Shape:")
+x = int(x)
+angle = 360 / x
+for i in range(x):
+    fd(50)
+    lt(angle)
+from turtle import *
+shape("turtle")
+f = numinput("F-фигура", "Введите F фигуру/Enter F Shape:")
+f = int(f)
+angle = 360 / f
+for i in range(f):
+    fd(70)
+    lt(angle)
+    from turtle import *
+
+    shape("turtle")
+
+    goto(200, 200)
+    color("slate blue")
+    begin_fill()
+    circle(50)
+    end_fill()
+    mainloop()
+
+    from turtle import *
+
+    shape("turtle")
+
+    goto(100, 100)
+    color("magenta")
+    begin_fill()
+    circle(50)
+    end_fill()
+    mainloop()
+print("Art")
+from turtle import *
+shape("circle")
+g = numinput("G-фигура", "Введите G фигуру/Enter G Shape:")
+g = int(g)
+angle = 360 / g
+for i in range(g):
+    fd(50)
+    lt(angle)
+
+
---
+++ Python Drawings.py
@@ -0,0 +1,60 @@
+from turtle import *
+shape("circle")
+for i in range(4):
+    fd(100)
+    lt(90)
+
+from turtle import *
+shape("circle")
+for i in range(5):
+    fd(90)
+    lt(80)
+    from turtle import *
+    shape("circle")
+    for i in range(6):
+        fd(80)
+        lt(80)
+from turtle import *
+shape("square")
+for i in range(5):
+    fd(80)
+    lt(70)
+
+    from turtle import *
+
+    shape("circle")
+    for i in range(5):
+        fd(90)
+        lt(80)
+    from turtle import *
+
+    shape("circle")
+    for i in range(6):
+        fd(60)
+        lt(50)
+    from turtle import *
+
+shape("circle")
+for i in range(7):
+    fd(50)
+    lt(40)
+    from turtle import *
+
+shape("circle")
+for i in range(8):
+    fd(40)
+    lt(30)
+    from turtle import *
+
+shape("circle")
+for i in range(9):
+    fd(30)
+    lt(20)
+    from turtle import *
+
+shape("turtle")
+for i in range(10):
+    fd(20)
+    lt(10)
+
+
